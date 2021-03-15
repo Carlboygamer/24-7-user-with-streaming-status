5d062ef2079fe4790de84ccec3403de8ed200ae4
Carlboygamer <79814454+Carlboygamer@users.noreply.github.com>
2021-03-15 11:15:59 +0800

    Add files via upload

---
+++ keep_alive.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def main():
+    return "Made by カズマ斉藤#9510 and enjoy the 24/7 user with streaming"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
