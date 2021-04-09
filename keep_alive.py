7b46e4b3dc8f7c94ad54689a4ccb8a095bee95a1
Carlboygamer <79814454+Carlboygamer@users.noreply.github.com>
2021-04-09 13:08:35 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return "Made by カズマ斉藤#4179and enjoy the 24/7 user with streaming"
+    return "Made by カズマ斉藤#4179 and enjoy the 24/7 user with streaming"
 
 def run():
     app.run(host="0.0.0.0", port=8080)
